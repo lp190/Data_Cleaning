0e2dde8bc4630b27812b7ab7689be45ef26a95cb
Lokesh Pandey <lokeshpandey190@gmail.com>
2022-10-29 09:13:20 +0200

    Update

--- 03_Dealing_Outliers.R
+++ 03_Dealing_Outliers.R
@@ -15,7 +15,7 @@ str(d2)
 
 ## Identifying outliers with visualization
 
-## Mathematically, outlier -> obs that is more than three
+## Mathematically, outlier -> obs that are more than three
 # standard deviations from the mean (sometimes 2.5 or 2)
 # Z-score
 
@@ -23,20 +23,26 @@ str(d2)
 ## For Univariate outliers, use boxplot from ggplolot
 
 ## Use box plot to check for outliers in Brazil's
-# refund values
+# refund data
 
-ggplot(d2, aes(x = "refund_value", y = refund_value)) + geom_boxplot()
+ggplot(d2, aes(x = "refund_value", y = refund_value)) + geom_boxplot() 
 
 ## Outliers look interesting, line near zero is the actual box
 
 
 ## Multivariate outliers (Categorical + numeric)
 ## now looking at the outliers for each different refund 
+ggplot(d2, aes(x = refund_description, y = refund_value)) + geom_boxplot() 
+# does not look that great
+
 ggplot(d2, aes(refund_description, refund_value)) + geom_boxplot() + coord_flip() 
   # coord_flip()  flips our plot for easy reading
   # looking at the above boxplot, dissemation of parliamentary activity has extreme
   # outlier
   # Similarly, mail costs and consulting, research ...
+
+# Depending on the parties
+ggplot(d2, aes(political_party, refund_value)) + geom_boxplot() + coord_flip()
 
 ## Multivariate outliers (Numeric + numeric); scatter plot
 ggplot(d2, aes(party_nmembers, refund_value)) + geom_point()
@@ -44,72 +50,84 @@ ggplot(d2, aes(party_nmembers, refund_value)) + geom_point()
 ## outliers in refund_value across the different 
 # number of party members
 
-#### WOrk with d1
+#### Work with d1
 ggplot(d1, aes(x = "receipt_value", y = receipt_value)) + geom_boxplot()
 
 ## Find receipt value according to receipt description
-ggplot(d1, aes(receipt_description, receipt_value)) + geom_point()
-
+ggplot(d1, aes(receipt_description, receipt_value)) + geom_point() + coord_flip()
+##########################################################################
 ##########################################################################
 
 
 ## Identifying rows containing outliers
-## Find z-score that are away from zero, What is z score?? USed to standardize
-##  Lukasz Komsta' s outliers package; score() can be used to find z score in 
+## Find z-score that are away from zero, What is z score?? Used to standardize
+##  Lukasz Komsta's outliers package; scores() can be used to find z score in 
 ## in every column; finding out the outliers column
 
-## Working with d1 again
-z_score_receipt <- scores(d1$receipt_value)
-# set a threshold of z-score making a new column out of them
-outl <- z_score_receipt > 3 | z_score_receipt < -3
-# creating a new column if refund values are outlier
-d1$outl <- outl
+## Working with d1 again; outliers for receipt_value
+zscore_d1 <- scores(d1$receipt_value) # calculate zscores of all receipt_value
+# create a categorical var such that zscore so -3 > that zscore >3  are outliers
+out_d1 <- zscore_d1 > 3 | zscore_d1 < -3
 
-## Working with d2 now
-zscore_refund <- scores(d2$refund_value)
-# Threshold
-outlier_d2 <-  zscore_refund > 3 | zscore_refund < -3
-d2$outlier_d2 <- outlier_d2
+# add the logical var into the dataset. add out_d1 into the main dataset
+d1$out_d1 <- out_d1
 
-#############################################
+## Now check the data out?? Check the end columns
+View(d1)
+############################################################
+############################################################
+
+## Working with d2 now; outliers for refund_value
+zscore_d2 <- scores(d2$refund_value)
+
+# Categorical var such that outliers (-3 > zscore > 3) true, else false
+out_d2 <- zscore_d2 > 3 | zscore_d2 < -3
+
+# mutating the categorical var to the main dataset d2
+d2$out_d2 <- out_d2
+############################################################
+##########################################################
+
+
 # Column of outliers are obtained in both datasets
-# Time to plot outlier and non-outlier values
+# Time to plot outlier and inlier values seperately
 
 # D1; receipt data first
-ggplot(d1, aes(receipt_description, receipt_value)) + 
-  geom_boxplot() + coord_flip() + facet_wrap(~outl)
-
-# d2: refund data second
-
-ggplot(d2, aes(refund_description, refund_value)) +
-  geom_boxplot() + coord_flip() + facet_wrap(~outlier_d2)
+ggplot(d1, aes(receipt_description, receipt_value )) + geom_boxplot() +
+  facet_wrap(~out_d1) + coord_flip()  # add coord_flip for inverting, readable
 
 
-## Another way of doing outliers
-d1_outliers <- d2[zscore_receipt > 3 | zscore_receipt < -3, ]
-head(d1_outliers)
+# D2: refund data second; same thing as in d1
 
-d2_outliers <- d1[z_score_refund > 3 | z_score_refund < -3, ]
-View(d2_outliers)
+ggplot(d2, aes(refund_description, refund_value)) + geom_boxplot() + 
+  facet_wrap(~out_d2) + coord_flip()
+
+
+## Another way of doing only outliers
+
+d1_out <- d1[zscore_d1 > 3 | zscore_d1 < -3, ]
+
+
+d2_out <- d2[zscore_d2 > 3 | zscore_d2 < -3, ]
 
 
 ########################################################################
 # Now that outliers have been identified, time to deal with them
 ########################################################################
 
-### Some strategies for handling outlier
+### Some strategies for handling outliers
 
-##### 1. Omitting the row with outlier. Recommended when:
+##### 1. Omitting the row with outliers. Recommended when:
 
-# - time contrainst to deal with them
+# - time constraint to deal with them
 # - large amount of data with no outliers
 # - outliers due to measurement or data entry errors
 
 #### Let's perform this in R
 ## Only select rows where there are no outliers acc. to the set threshold
+## Dropping obs with outliers, not the best solution
 
-rem_outlier_d1 <- d1[d1$outl == F,]
-
+no_outliers_d1 <- d1[d1$out_d1 == F,]
 # 2975608 obs remaining from 3014902
 # dropped 39294 obs
 
@@ -122,31 +140,76 @@ rem_outlier_d1 <- d1[d1$outl == F,]
 # - a lot of time to discover and analyze outliers
 
 # Code to make subset of outliers and inliers
+# outliers
+d1_out <- d1[zscore_d1 > 3 | zscore_d1 < -3] # only outliers
 
-d1_outliers <- d1[z_score_receipt > 3 | z_score_receipt  < -3,] #only outliers
-View(d1_outliers)
-
-d1_inliers <- d1[z_score_receipt < 3 & z_score_receipt > -3,] # only inliers
-View(d1_inliers)
-
+# inliers only
+d1_in <- d1[d1$out_d1 == F,]
 
 ##### 3. Remove and replace; imputation
 # Outlier could be because of error or missing values
 # maynot represent the true values
-# They could then me omitted and imputed like the missing values. RECOMMENDED when:
+# They could then be omitted and imputed like the missing values. RECOMMENDED when:
 
 # - Outliers due to measurement errors
 # - NAs are assigned with values for eg: -99999, which are obv. outliers
-## Coding time; replace outliers with NA
-d1[z_score_receipt > 3 | z_score_receipt < -3, "receipt_value"] <- NA
-summary(d1$receipt_value) # checks for NAs
 
+## Coding time; replace outliers with NA
+d1[zscore_d1 >3 | zscore_d1 < -3, "receipt_value"] <- NA
+
+
+
+summary(d1$receipt_value) # checks for NAs
+# 39294 NAs
 # Converted to NA and now deal with the NAs
 
+# Maybe try and save NA -ed  dataset
+
+# try 
+## 1. write.table() 
+
+write.table(d1, file = "NA-ed_d1.csv",
+            sep = "\t", row.names = F)
+
+### Works  
+
+## 2. fwrite() from data.table package if the data is huge
+## 3. library(WriteXLS)
+
+
+ 
+# Check it out again, csv looks weird 
 
 #####################################################################
 
 
 
 ##### Deal with outliers in d2; refund data
+## There are three options.
+## Option 1: eliminating the obs with outliers. 
+# Coding time baby
 
+no_outlier_d2 <- d2[d2$out_d2 == F,]
+
+# resulting dataset has 333769 out of d2 = 339089 obs, 5329 obs dropped
+## this could be used for empirical analysis
+
+## Option 2: subsetting outliers and inliers and studying them individually
+## Coding time baby
+# d2 outliers
+d2_out <- d2[zscore_d2 > 3 | zscore_d2 < -3,]
+
+# d2 inliers
+d2_in <- d2[d2$out_d2 == F,]
+
+## After seperating outliers and inliers, independent study can be done, alright!
+
+
+## Option 3: assigning NA to "outliers and imputing them later
+
+d2[zscore_d2 > 3 | zscore_d2 < - 3, "refund_value"] <- NA
+
+## Save the NA-ed file
+
+write.table(d2, file = "NA-ed_d2.csv",
+            sep = "\t", row.names = F)
